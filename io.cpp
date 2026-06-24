1af1e8bd8fe2cca3aa72c61a39960f74b85c2270
dritireddy <driti2828@gmail.com>
2026-06-24 18:26:41 +0530

    Add files via upload

---
+++ io.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n1,n2;
+    cout<<"enter n1 and n2";
+    cin>>n1>>n2;
+    cout<<"sum="<<n1+n2;
+    return 0;
+
+}
---
+++ op.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float a=4.5;
+    cout<<a<<endl;
+    cout<<(int)a;
+    return 0;
+
+}
